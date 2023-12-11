f98f80313c977d7402b5b3a5b7a41b96abbad2a2
hanmin <gksals9563@naver.com>
2023-12-11 19:42:33 +0900

    백준첫기록

---
+++ 수행시간.py
@@ -0,0 +1,17 @@
+num = ['0','1','2','3','4','5','6','7','8','9']
+sums = 0
+sumstr =[]
+s = input()
+
+for i in s:
+    if i in num:
+        sums += int(i)
+    else:
+        sumstr.append(i)
+sumstr.sort()
+
+sumstr.append(str(sums))
+
+print(''.join(sumstr))
+
+
